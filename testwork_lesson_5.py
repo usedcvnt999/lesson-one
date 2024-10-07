ec8a6e5df4a884f240428749116e23789d7edddf
usedcvnt999 <nantixgg@gmail.com>
2024-10-07 21:31:12 +0300

    testwork 5

--- testwork_lesson_5.py
+++ testwork_lesson_5.py
@@ -1,30 +1,15 @@
-# the_boys = {
-#     'name': 'The Boys',
-#     'releaseDate': 'July 26, 2019',
-#     'mainActor': {
-#         'Homelander': 'Antony Starr',
-#         'AnnieJanuary': 'Erin Moriarty',
-#         'BillyButcher': 'Karl Urban',
-#         'HughieCampbell': 'Jack Quaid',
-#     },
-#     'episodes': 32
-# }
-#
-# print(f' Сериал {the_boys['name']}, был выпущен {the_boys['releaseDate']} года.\n В главных ролях {the_boys['mainActor']}.\n В {the_boys['name']} {the_boys['episodes']} эпизодов или серий')
-from math import expm1
-
-students = {
-    1: {
-        "Name": "Maksim",
-        "Age": 45,
-        "Subjects": ["Algebra", "Physics"]
+the_boys = {
+    'name': 'The Boys',
+    'releaseDate': 'July 26, 2019',
+    'mainActor': {
+        'Homelander': 'Antony Starr',
+        'AnnieJanuary': 'Erin Moriarty',
+        'BillyButcher': 'Karl Urban',
+        'HughieCampbell': 'Jack Quaid',
     },
-    2: {
-        "Name": "Eugen",
-        "Age": 17,
-        "Subjects": ["History", "Biology"]
-    }
+    'episodes': 32
 }
 
+print(f' Сериал {the_boys['name']}, был выпущен {the_boys['releaseDate']} года.\n В главных ролях {the_boys['mainActor']}.\n В {the_boys['name']} {the_boys['episodes']} эпизодов или серий')
 
 
